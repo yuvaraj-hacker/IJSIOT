94b26bbfceb8f63bdff05c81669314f5cdf5936d
vetri <vetrivel.tcz@gmail.com>
2025-04-03 18:41:48 +0530

    change

--- src/Shared/Components/Copyrights/Copyrights.jsx
+++ src/Shared/Components/Copyrights/Copyrights.jsx
@@ -71,7 +71,7 @@ function Copyrights() {
                                 </h1>
                             </div>
                             <p className="text-justify md:text-base text-sm">
-                                I/We ____________________________, the author(s) of the research paper/article entitled ______________________, hereby grant permission for the publication of the above-mentioned paper/article in IJSIOT (International Journal of Smart Internet of Things and Applications).
+                                I/We ____________________________, the author(s) of the research paper/article entitled ______________________, hereby grant permission for the publication of the above-mentioned paper/article in International Journal of Smart Internet of Things and Applications.
                             </p>
                             <p className="text-justify md:text-base text-sm">
                                 <p className=""> It is agreed that :</p>
